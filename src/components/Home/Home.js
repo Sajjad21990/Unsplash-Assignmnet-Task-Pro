73f42524ea5340b04923164a41af1b892a09550d
Sajjad21990 <sajjadhaider21990@gmail.com>
2021-01-03 16:33:15 +0530

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
   useEffect(() => {
     localStorage.setItem("query", "");
     fetchImages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchImages = async () => {
